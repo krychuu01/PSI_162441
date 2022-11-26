32f5ff2b70fbf1e30528ee89bc133ebfa7f4a0bb
GKrych <hazzidy125@gmail.com>
2022-11-26 01:03:22 +0100

    changes in create_database.sql file

--- src/main/resources/flyway_scripts/V1__create_database.sql
+++ src/main/resources/flyway_scripts/V1__create_database.sql
@@ -12,8 +12,7 @@ CREATE TABLE IF NOT EXISTS `bookstore`.`books` (
   `publisher` VARCHAR(45) NOT NULL,
   `binding` ENUM("softcover", "hardcover", "e_book") NOT NULL,
   PRIMARY KEY (`id`))
-  AUTO_INCREMENT = 10000
-  ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+  AUTO_INCREMENT = 10000;
 
 CREATE TABLE IF NOT EXISTS `bookstore`.`addresses` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
@@ -22,8 +21,7 @@ CREATE TABLE IF NOT EXISTS `bookstore`.`addresses` (
   `city` VARCHAR(45) NULL,
   `country` VARCHAR(45) NULL,
   PRIMARY KEY (`id`))
-  AUTO_INCREMENT = 10000
-  ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+  AUTO_INCREMENT = 10000;
 
 CREATE TABLE IF NOT EXISTS `bookstore`.`users` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
@@ -35,16 +33,10 @@ CREATE TABLE IF NOT EXISTS `bookstore`.`users` (
   `phone_number` VARCHAR(9) NOT NULL,
   `date_of_birth` DATE NOT NULL,
   `addresses_id` BIGINT NOT NULL,
-  PRIMARY KEY (`id`, `addresses_id`),
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
-  INDEX `fk_users_addresses1_idx` (`addresses_id` ASC) VISIBLE,
-  CONSTRAINT `fk_users_addresses1`
-    FOREIGN KEY (`addresses_id`)
-    REFERENCES `bookstore`.`addresses` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-    AUTO_INCREMENT = 10000
-    ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+  PRIMARY KEY (`id`),
+  foreign key (`addresses_id`) references addresses(id))
+  auto_increment = 10000;
+
 
 CREATE TABLE IF NOT EXISTS `bookstore`.`orders` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
@@ -52,15 +44,8 @@ CREATE TABLE IF NOT EXISTS `bookstore`.`orders` (
   `order_date` DATE NOT NULL,
   `total_price` DOUBLE NOT NULL,
   `users_id` BIGINT NOT NULL,
-  PRIMARY KEY (`id`, `users_id`),
-  INDEX `fk_orders_users1_idx` (`users_id` ASC) VISIBLE,
-  CONSTRAINT `fk_orders_users1`
-    FOREIGN KEY (`users_id`)
-    REFERENCES `bookstore`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-    AUTO_INCREMENT = 10000
-    ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+  PRIMARY KEY (`id`, `users_id`))
+  AUTO_INCREMENT = 10000;
 
 
 CREATE TABLE IF NOT EXISTS `bookstore`.`authors` (
@@ -68,26 +53,13 @@ CREATE TABLE IF NOT EXISTS `bookstore`.`authors` (
   `first_name` VARCHAR(45) NOT NULL,
   `last_name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`))
-  ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+  auto_increment = 10000;
 
 CREATE TABLE IF NOT EXISTS `bookstore`.`books_has_authors` (
   `books_id` BIGINT NOT NULL,
   `authors_id` BIGINT NOT NULL,
-  PRIMARY KEY (`books_id`, `authors_id`),
-  INDEX `fk_books_has_autors_autors1_idx` (`authors_id` ASC) VISIBLE,
-  INDEX `fk_books_has_autors_books1_idx` (`books_id` ASC) VISIBLE,
-  CONSTRAINT `fk_books_has_autors_books1`
-    FOREIGN KEY (`books_id`)
-    REFERENCES `bookstore`.`books` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_books_has_autors_autors1`
-    FOREIGN KEY (`authors_id`)
-    REFERENCES `bookstore`.`authors` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-    AUTO_INCREMENT = 10000
-    ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+  PRIMARY KEY (`books_id`, `authors_id`))
+  AUTO_INCREMENT = 10000;
 
 
 CREATE TABLE IF NOT EXISTS `bookstore`.`orders_info` (
@@ -97,17 +69,6 @@ CREATE TABLE IF NOT EXISTS `bookstore`.`orders_info` (
   `books_amount` INT NOT NULL,
   `price` DOUBLE NOT NULL,
   PRIMARY KEY (`id`, `orders_id`, `books_id`),
-  INDEX `fk_orders_has_books_books1_idx` (`books_id` ASC) VISIBLE,
-  INDEX `fk_orders_has_books_orders1_idx` (`orders_id` ASC) VISIBLE,
-  CONSTRAINT `fk_orders_has_books_orders1`
-    FOREIGN KEY (`orders_id`)
-    REFERENCES `bookstore`.`orders` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_orders_has_books_books1`
-    FOREIGN KEY (`books_id`)
-    REFERENCES `bookstore`.`books` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-    AUTO_INCREMENT = 10000
-    ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+  foreign key (`orders_id`) references orders(id),
+  foreign key (`books_id`) references books(id))
+  AUTO_INCREMENT = 10000;
